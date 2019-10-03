75daddec45f3c5a5d9c5c01cea20dcebd613d5e6
YuhaoZhang-tirsky <tirsky@outlook.com>
2019-10-03 23:10:49 +1000

    bug fixing

--- client/script.js
+++ client/script.js
@@ -5,10 +5,11 @@
 
     async function remoteCall(input) {
         document.getElementById('dropdown-content').innerHTML = '';
+        document.getElementById('dropdown-menu').classList.add('is-hidden');
         var response = await fetch(`${process.env.ENDPOINT}?app_id=${process.env.APP_ID}&app_code=${process.env.APP_CODE}&query=${req.params.query}&maxresults=5&country=AUS`)
         var responseJson = await response.json();
         responseJson.suggestions.forEach(item => {
-            document.getElementById('dropdown-content').insertAdjacentHTML('beforeend', `<a href="#" class="dropdown-item">
+            document.getElementById('dropdown-content').insertAdjacentHTML('beforeend', `<a href="#" onClick="setInputValue(this)" class="dropdown-item">
             ${item.label}
           </a>`)
         });
