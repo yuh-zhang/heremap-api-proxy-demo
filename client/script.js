c535ed62245a069485dc1e36264c2cdab754828b
YuhaoZhang-tirsky <tirsky@outlook.com>
2019-10-04 01:02:22 +1000

    bug fixing

--- client/script.js
+++ client/script.js
@@ -1,25 +1,37 @@
 (function () {
     document.getElementById('autocomplete-input').addEventListener('keyup', (ele) => {
-        document.getElementById('control').classList.add('is-loading');
-        debounce(remoteCall, 1000)(ele.target.value);
-    })
-
-    async function remoteCall(input) {
-        document.getElementById('dropdown-content').innerHTML = '';
+        document.getElementById('autocomplete-control').classList.add('is-loading');
+        debounce(fireSearch, 1000)(ele.target.value);
+    });
+    const fireSearch = async (input) => {
+        document.getElementById('autocomplete-dropdown-items').innerHTML = '';
         document.getElementById('dropdown-menu').classList.add('is-hidden');
-        var response = await fetch(`/api/autocomplete/${input}`)
-        console.log(response)
-        var responseJson = await response.json()
-        console.log(responseJson)
-        responseJson.suggestions.forEach(item => {
-            document.getElementById('dropdown-content').insertAdjacentHTML('beforeend', `<a href="#" onClick="setInputValue(this)" class="dropdown-item">
-            ${item.label}
-          </a>`)
+
+        const response = await remoteCall(input)
+        document.getElementById('autocomplete-control').classList.remove('is-loading');
+
+        if (!response.suggestions) {
+            document.getElementById('dropdown-menu').classList.add('is-hidden');
+            return;
+        }
+        response.suggestions.forEach(item => {
+            document.getElementById('dropdown-menu').classList.remove('is-hidden');
+            document.getElementById('autocomplete-dropdown-items')
+                .insertAdjacentHTML('beforeend', `
+                        <a href="#" onClick="setInputValue(this)" class="dropdown-item">
+                            ${item.label}
+                        </a>
+                    `)
         });
     }
 
+    async function remoteCall(input) {
+        const response = await fetch(`/api/autocomplete/${input}`);
+        return await response.json();
+    }
     var inDebounce
     const debounce = (func, delay) => {
+
         return function () {
             const context = this
             const args = arguments
@@ -27,6 +39,7 @@
             inDebounce = setTimeout(() => func.apply(context, args), delay)
         }
     }
+
     var inThrottle
     const throttle = (func, limit) => {
         return function () {
@@ -40,3 +53,13 @@
         }
     }
 }());
+function setInputValue(ele) {
+    document.getElementById('autocomplete-input').value = ele.innerText
+    document.getElementById('autocomplete-dropdown-items').innerHTML = '';
+    document.getElementById('dropdown-menu').classList.add('is-hidden');
+
+}
+
+function testAlert() {
+    alert('test')
+}
