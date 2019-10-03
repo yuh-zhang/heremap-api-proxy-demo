081f7c39cfc60e05179ddb57c02cdbf44ddc74ae
YuhaoZhang-tirsky <tirsky@outlook.com>
2019-10-03 22:46:02 +1000

    bug fixing

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = require('./routes/router')
+const router = require('./routers/router')
 require('dotenv').config()
 const app = express();
 const PORT = process.env.PORT || 3001
